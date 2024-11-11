070c452c2dc5ebe495183012ac515e5d4b52d6f5
Jo Vo <jotanvo@gmail.com>
2024-11-11 20:53:14 +0100

    Re-organized file structure attempt 1#4

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
         emptyOutDir: true,
         rollupOptions: {
            input: {
-               main: resolve(root, 'index.html'),
-               feed: resolve(root, 'feed', 'index.html'),
-               profile: resolve(root, 'profile', 'index.html'),
+               main: resolve(root, 'profile.html'),
+               feed: resolve(root, 'feed', 'profile.html'),
+               profile: resolve(root, 'profile', 'profile.html'),
            }
         }
     }
